066ff4e9a9c384ee4a2f0a648dcccbfb8f2c684b
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-14 20:01:02 -0300

    feat(src/modules/users): create loginUserController

---
+++ src/modules/users/useCases/loginUser/loginUserController.ts
@@ -0,0 +1,21 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import LoginUserUseCase from './loginUserUseCase';
+
+export default class LoginUserController {
+  private loginUserUseCase: LoginUserUseCase;
+
+  constructor() {
+    this.loginUserUseCase = new LoginUserUseCase();
+  }
+
+  async login(req: Request, res: Response, next: NextFunction) {
+    const { email, password } = req.body;
+    try {
+      const result = await this.loginUserUseCase.login({ email, password });
+      return res.status(StatusCodes.OK).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
