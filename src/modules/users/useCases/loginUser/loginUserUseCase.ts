1e19f2cf7cc828fa6ac5483596c8b458600c3f35
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-14 20:01:28 -0300

    feat(src/modules/users): create class LoginUserUseCase

---
+++ src/modules/users/useCases/loginUser/loginUserUseCase.ts
@@ -0,0 +1,33 @@
+import errors from 'restify-errors';
+import Bcrypt from '../../../../helpers/bcrypt';
+import Token from '../../../../helpers/jwt.auth';
+import UsersRepository from '../../../../repositories/implementations/usersRepository';
+import { login } from '../../../../types/users';
+
+export default class LoginUserUseCase {
+  private userRepository: UsersRepository;
+
+  constructor() {
+    this.userRepository = new UsersRepository();
+  }
+
+  async login({ email, password }: login): Promise<object> {
+    const user = await this.userRepository.findByEmail(email);
+    const passwordUser = user ? user.password : '';
+    const passwordValidate = await Bcrypt.validate(password, passwordUser);
+    if (!user || !passwordValidate) {
+      throw new errors.UnauthorizedError('Incorrect email or password');
+    }
+    const { first_name: firstName, last_name: lastName, id } = user;
+    const token = Token.generate({
+      id, firstName, lastName,
+    });
+
+    return {
+      user: {
+        id, firstName, lastName, email,
+      },
+      token,
+    };
+  }
+}
