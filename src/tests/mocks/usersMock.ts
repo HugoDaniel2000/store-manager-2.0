6afb9657e0adf784d4f661917f1dde2df6f76373
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-15 04:25:17 -0300

    style(src/test): add attribute role

--- src/tests/mocks/usersMock.ts
+++ src/tests/mocks/usersMock.ts
@@ -4,6 +4,7 @@ const user: User = {
   id: 1,
   first_name: 'Hugo Daniel',
   last_name: 'Caxias das Virgens',
+  role: 'customer',
   password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
   email: 'hugodaniel@gmail.com',
 };
@@ -12,6 +13,7 @@ const userCreated: userCreated = {
   id: 1,
   firstName: 'Hugo Daniel',
   lastName: 'Caxias das Virgens',
+  role: 'customer',
   email: 'hugodaniel@gmail.com',
 };
 
