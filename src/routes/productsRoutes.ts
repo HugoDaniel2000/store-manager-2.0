9d46ac3f7d135e0029e2ad43fab738a9d21f1db5
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-15 18:20:28 -0300

    feat(src/routes): add create products controller in the route (/)

--- src/routes/productsRoutes.ts
+++ src/routes/productsRoutes.ts
@@ -1,16 +1,26 @@
 import {
   NextFunction, Response, Request, Router,
 } from 'express';
+import AuthMiddleware from '../middlewares/authMiddleware';
 
 import FindProductsController from '../modules/products/useCase/findProducts/findProductsController';
+import ProductsController from '../modules/products/useCase/product/productsController';
 
 const findProductsController = new FindProductsController();
+const productsController = new ProductsController();
+
 const router = Router();
 
 router.route('/')
   .get((req: Request, res: Response, next: NextFunction) => {
     findProductsController.findAllProducts(req, res, next);
-  });
+  })
+  .post(
+    AuthMiddleware.validToken,
+    (req: Request, res: Response, next: NextFunction) => {
+      productsController.createProducts(req, res, next);
+    },
+  );
 
 router.route('/:id')
   .get((req: Request, res: Response, next: NextFunction) => {
