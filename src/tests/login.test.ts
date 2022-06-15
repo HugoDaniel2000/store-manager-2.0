2a72efea876bc1b8e99b84daf66b109b39ccc909
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-14 22:20:11 -0300

    feat(src/test): add test falid login

--- src/tests/login.test.ts
+++ src/tests/login.test.ts
@@ -9,11 +9,6 @@ import { user } from './mocks/usersMock';
 import { User } from '../types/users';
 import Bcrypt from '../helpers/bcrypt';
 
-// import Users from '../database/models/users.model';
-// import Token from '../helpers/jwt.auth';
-// import Encrypt from '../helpers/encrypt';
-// import { users } from './mocks/users';
-
 chai.use(chaiHttp);
 
 const { expect } = chai;
@@ -21,36 +16,86 @@ const { expect } = chai;
 describe('rota login/', async () => {
   let chaiHttpResponse: Response;
   let repositoryStub: sinon.SinonStub;
+  describe('login success', async () => {
+    beforeEach(async () => {
+      repositoryStub = sinon.stub(UsersRepository.prototype, 'findByEmail').resolves(user as User);
+      sinon.stub(Bcrypt, 'validate').resolves(true);
+    });
+    afterEach(() => {
+      repositoryStub.restore();
+      (Bcrypt.validate as sinon.SinonStub).restore();
+    });
+    it('deve retorna status 200', async () => {
+      chaiHttpResponse = await chai
+        .request(app).post('/login')
+        .send({
+          email: 'hugodaniel@gmail.com',
+          password: '123456789',
+        });
 
-  describe('Realiza login', async () => {
-    describe('login success', async () => {
-      // const tokenValidate = 'abc.def.ghi';
-      beforeEach(async () => {
-        repositoryStub = sinon.stub(UsersRepository.prototype, 'findByEmail').resolves(user as User);
-        sinon.stub(Bcrypt, 'validate').resolves(true);
-      });
-      afterEach(() => {
-        repositoryStub.restore();
-        (Bcrypt.validate as sinon.SinonStub).restore();
-      });
-      it('deve retorna status 200', async () => {
-        chaiHttpResponse = await chai
-          .request(app).post('/login')
-          .send({
-            email: 'hugodaniel@gmail.com',
-            password: '123456789',
-          });
+      expect(chaiHttpResponse.status).to.be.eql(200);
+      expect(chaiHttpResponse.body).to.be.an('object');
+      expect(chaiHttpResponse.body).to.have.property('user');
+      expect(chaiHttpResponse.body).to.have.property('token');
+      expect(chaiHttpResponse.body.user).to.be.an('object');
+      expect(chaiHttpResponse.body.user).to.have.property('id');
+      expect(chaiHttpResponse.body.user).to.have.property('firstName');
+      expect(chaiHttpResponse.body.user).to.have.property('lastName');
+      expect(chaiHttpResponse.body.user).to.have.property('email');
+    });
+  });
 
-        expect(chaiHttpResponse.status).to.be.eql(200);
-        expect(chaiHttpResponse.body).to.be.an('object');
-        expect(chaiHttpResponse.body).to.have.property('user');
-        expect(chaiHttpResponse.body).to.have.property('token');
-        expect(chaiHttpResponse.body.user).to.be.an('object');
-        expect(chaiHttpResponse.body.user).to.have.property('id');
-        expect(chaiHttpResponse.body.user).to.have.property('firstName');
-        expect(chaiHttpResponse.body.user).to.have.property('lastName');
-        expect(chaiHttpResponse.body.user).to.have.property('email');
-      });
+  describe('login falid', async () => {
+    beforeEach(async () => {
+      repositoryStub = sinon.stub(UsersRepository.prototype, 'findByEmail').resolves(user as User);
+      sinon.stub(Bcrypt, 'validate').resolves(false);
+    });
+    afterEach(() => {
+      repositoryStub.restore();
+      (Bcrypt.validate as sinon.SinonStub).restore();
+    });
+
+    it('email format invalid', async () => {
+      chaiHttpResponse = await chai
+        .request(app).post('/login')
+        .send({
+          email: 'hugodanielgmail.com',
+          password: '123456789',
+        });
+
+      expect(chaiHttpResponse.status).to.be.eql(400);
+      expect(chaiHttpResponse.body).to.eql({ message: 'Email must have a valid format' });
+    });
+
+    it('password invalid', async () => {
+      chaiHttpResponse = await chai
+        .request(app).post('/login')
+        .send({
+          email: 'hugodaniel@gmail.com',
+          password: '123456789',
+        });
+
+      expect(chaiHttpResponse.status).to.be.eql(401);
+      expect(chaiHttpResponse.body).to.eql({ message: 'Incorrect email or password' });
+    });
+    it('fields password empty', async () => {
+      chaiHttpResponse = await chai
+        .request(app).post('/login')
+        .send({
+          email: 'hugodaniel@gmail.com',
+        });
+      expect(chaiHttpResponse.status).to.be.eql(400);
+      expect(chaiHttpResponse.body).to.eql({ message: 'All fields must be filled' });
+    });
+
+    it('fields email empty', async () => {
+      chaiHttpResponse = await chai
+        .request(app).post('/login')
+        .send({
+          password: '1234567',
+        });
+      expect(chaiHttpResponse.status).to.be.eql(400);
+      expect(chaiHttpResponse.body).to.eql({ message: 'All fields must be filled' });
     });
   });
 });
