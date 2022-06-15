a789c5b2e778a689ad0c77cadea220bd9c72cc99
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-15 19:25:41 -0300

    test(src/modules/products): add create products crash tests

--- src/modules/products/tests/createProducts.test.ts
+++ src/modules/products/tests/createProducts.test.ts
@@ -17,16 +17,19 @@ const { expect } = chai;
 describe('Products', async () => {
   let chaiHttpResponse: Response;
   let repositoryStub: sinon.SinonStub;
+  let repositoryStub2: sinon.SinonStub;
   let tokenStub: sinon.SinonStub;
 
   describe('Create products', async () => {
     describe('create products success', async () => {
       beforeEach(async () => {
         repositoryStub = sinon.stub(ProductsRepository.prototype, 'create').resolves(product as Products);
+        repositoryStub2 = sinon.stub(ProductsRepository.prototype, 'findByName').resolves(null);
         tokenStub = sinon.stub(Token, 'validate').returns(tokenPayloadAdmin);
       });
       afterEach(() => {
         repositoryStub.restore();
+        repositoryStub2.restore();
         tokenStub.restore();
       });
       it('return status 200', async () => {
@@ -52,7 +55,7 @@ describe('Products', async () => {
         repositoryStub.restore();
         tokenStub.restore();
       });
-      it('return status 401', async () => {
+      it('User not allowed', async () => {
         chaiHttpResponse = await chai
           .request(app)
           .post('/products')
@@ -64,6 +67,32 @@ describe('Products', async () => {
 
         expect(chaiHttpResponse.status).to.be.eql(401);
         expect(chaiHttpResponse.body).to.be.eql({ message: 'You do not have permission to create products' });
+      });
+    });
+
+    describe('Create products falid', async () => {
+      beforeEach(async () => {
+        repositoryStub = sinon.stub(ProductsRepository.prototype, 'create').resolves(product as Products);
+        repositoryStub2 = sinon.stub(ProductsRepository.prototype, 'findByName').resolves(product);
+        tokenStub = sinon.stub(Token, 'validate').returns(tokenPayloadAdmin);
+      });
+      afterEach(() => {
+        repositoryStub.restore();
+        repositoryStub2.restore();
+        tokenStub.restore();
+      });
+      it('Product already exists', async () => {
+        chaiHttpResponse = await chai
+          .request(app)
+          .post('/products')
+          .set('authorization', '123.456.789')
+          .send({
+            name: 'Vapor Max',
+            quantity: 10,
+          });
+
+        expect(chaiHttpResponse.status).to.be.eql(409);
+        expect(chaiHttpResponse.body).to.be.eql({ message: 'Product already exists' });
       });
     });
   });
