d23d9ab774787f037f0989b9be4f9118f15c42d0
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-15 04:23:52 -0300

    feat(src/modules/users): feat method update user

--- src/modules/users/useCases/user/userController.ts
+++ src/modules/users/useCases/user/userController.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
-import { userCreate } from '../../../../types/users';
+import { userCreate, userCreated } from '../../../../types/users';
 import UserUseCase from './userUseCase';
 
 export default class UserController {
@@ -15,6 +15,19 @@ export default class UserController {
     try {
       const result = await this.userUseCase.createUser(newUser);
       return res.status(StatusCodes.CREATED).json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateUser(req: Request, res: Response, next: NextFunction) {
+    const { id } = req.params;
+    const user = req.query.user as unknown as userCreated;
+
+    const userUpdate = req.body;
+    try {
+      const result = await this.userUseCase.updateUser({ id: Number(id), ...userUpdate }, user);
+      return res.status(StatusCodes.OK).json(result);
     } catch (error) {
       next(error);
     }
--- src/modules/users/useCases/user/userUseCase.ts
+++ src/modules/users/useCases/user/userUseCase.ts
@@ -1,7 +1,7 @@
-import erros from 'restify-errors';
+import errors from 'restify-errors';
 import Bcrypt from '../../../../helpers/bcrypt';
 import UsersRepository from '../../../../repositories/implementations/usersRepository';
-import { userCreate, userCreated } from '../../../../types/users';
+import { userCreate, userCreated, userUpdate } from '../../../../types/users';
 
 export default class UserUseCase {
   private userRepository: UsersRepository;
@@ -13,16 +13,33 @@ export default class UserUseCase {
   async createUser(newUser: userCreate): Promise<userCreated> {
     const userExist = await this.userRepository.findByEmail(newUser.email);
     if (userExist) {
-      throw new erros.ConflictError('Email is already being used');
+      throw new errors.ConflictError('Email is already being used');
     }
     const passwordEncrypted = await Bcrypt.encrypt(newUser.password);
     const user = newUser;
     user.password = passwordEncrypted;
     const {
-      id, last_name: lastName, first_name: firstName, email,
+      id, last_name: lastName, first_name: firstName, email, role,
     } = await this.userRepository.create(user);
     return {
-      id, lastName, firstName, email,
+      id, lastName, firstName, email, role,
+    };
+  }
+
+  async updateUser(user: userUpdate, loggedUser: userCreated): Promise<userCreated> {
+    const userAtt = user;
+    if (loggedUser.id !== user.id && loggedUser.role !== 'admin') {
+      throw new errors.UnauthorizedError('You do not have permission to update this user');
+    }
+    if (userAtt.password) {
+      const passwordEncrypted = await Bcrypt.encrypt(userAtt.password);
+      userAtt.password = passwordEncrypted;
+    }
+    const {
+      id, last_name: lastName, first_name: firstName, email, role,
+    } = await this.userRepository.update(userAtt);
+    return {
+      id, lastName, firstName, email, role,
     };
   }
 }
