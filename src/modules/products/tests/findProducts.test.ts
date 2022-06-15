9cdf40a2e7b11af4e764188ffaba0e0af75665b9
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-15 16:44:29 -0300

    test(src/modules/products): create tests and mocks find products

---
+++ src/modules/products/tests/findProducts.test.ts
@@ -0,0 +1,52 @@
+import sinon from 'sinon';
+import chai from 'chai';
+
+import chaiHttp = require('chai-http');
+import { Response } from 'superagent';
+import { Products } from '@prisma/client';
+import { app } from '../../../index';
+import ProductsRepository from '../../../repositories/implementations/productsRepository';
+import { product } from './mocks/productsMocks';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Products', async () => {
+  let chaiHttpResponse: Response;
+  let repositoryStub: sinon.SinonStub;
+
+  describe('Find by id products', async () => {
+    describe('Find by id products success', async () => {
+      beforeEach(async () => {
+        repositoryStub = sinon.stub(ProductsRepository.prototype, 'findById').resolves(product as Products);
+      });
+      afterEach(() => {
+        repositoryStub.restore();
+      });
+      it('return status 200', async () => {
+        chaiHttpResponse = await chai
+          .request(app).get('/products/1')
+          .send();
+        expect(chaiHttpResponse.status).to.be.eql(200);
+        expect(chaiHttpResponse.body).to.be.eql(product);
+      });
+    });
+
+    describe('Find by id products falid', async () => {
+      beforeEach(async () => {
+        repositoryStub = sinon.stub(ProductsRepository.prototype, 'findById').resolves(null);
+      });
+      afterEach(() => {
+        repositoryStub.restore();
+      });
+      it('return status 404', async () => {
+        chaiHttpResponse = await chai
+          .request(app).get('/products/1')
+          .send();
+        expect(chaiHttpResponse.status).to.be.eql(404);
+        expect(chaiHttpResponse.body).to.be.eql({ message: 'Product not found' });
+      });
+    });
+  });
+});
---
+++ src/modules/products/tests/mocks/productsMocks.ts
@@ -0,0 +1,8 @@
+const product = {
+  id: 1,
+  name: 'Vapor Max',
+  quantity: 10,
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { product };
