c9786cd610b33892045f8b3c1ea77423cd5db934
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-14 22:27:20 -0300

    feat(src/test): add mock user

--- src/tests/mocks/usersMock.ts
+++ src/tests/mocks/usersMock.ts
@@ -1,4 +1,4 @@
-import { User } from '../../types/users';
+import { User, userCreated } from '../../types/users';
 
 const user: User = {
   id: 1,
@@ -7,5 +7,13 @@ const user: User = {
   password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
   email: 'hugodaniel@gmail.com',
 };
+
+const userCreated: userCreated = {
+  id: 1,
+  firstName: 'Hugo Daniel',
+  lastName: 'Caxias das Virgens',
+  email: 'hugodaniel@gmail.com',
+};
+
 // eslint-disable-next-line import/prefer-default-export
-export { user };
+export { user, userCreated };
