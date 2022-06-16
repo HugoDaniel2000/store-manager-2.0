0a1a608dc898a259dae7f587e60492063c92b6ca
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-16 16:17:44 -0300

    feat(src/middlewares): add conditionals in middleware products

--- src/middlewares/productsMiddlewares.ts
+++ src/middlewares/productsMiddlewares.ts
@@ -10,11 +10,19 @@ export default class ProductMiddleware {
         throw new errors.BadRequestError('All fields must be filled');
       }
 
-      if (!product.quantity) {
+      if (typeof product.name !== 'string') {
+        throw new errors.BadRequestError('The name field must be of type string');
+      }
+
+      if (!product.quantity && typeof product.quantity !== 'number') {
         throw new errors.BadRequestError('All fields must be filled');
       }
 
-      if (product.quantity < 0) {
+      if (typeof product.quantity !== 'number') {
+        throw new errors.BadRequestError('The name field must be of type number');
+      }
+
+      if (product.quantity < 1) {
         throw new errors.BadRequestError('Quantity must be greater than to 0');
       }
     });
@@ -22,9 +30,21 @@ export default class ProductMiddleware {
   };
 
   public static validateUpdateProducts = (req: Request, res: Response, next: NextFunction) => {
-    const { name } = req.body;
-    if (name && name.length === 0) {
-      throw new errors.BadRequestError('All fields must be filled');
+    const { name, quantity } = req.body;
+    if (name && typeof name !== 'string') {
+      throw new errors.BadRequestError('The name field must be of type string');
+    }
+
+    if (name === '') {
+      throw new errors.BadRequestError('The name field must be filled');
+    }
+
+    if (typeof quantity !== 'number') {
+      throw new errors.BadRequestError('The name field must be of type number');
+    }
+
+    if (quantity < 0) {
+      throw new errors.BadRequestError('Quantity must be greater than or equal to 0');
     }
 
     next();
