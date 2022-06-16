00c994efbe1af76de865e8ff6a3a115d6568f086
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-16 16:50:00 -0300

    test(src/modules/sales): create test find all and find by id sales

---
+++ src/modules/sales/tests/findSales.test.ts
@@ -0,0 +1,73 @@
+import sinon from 'sinon';
+import chai from 'chai';
+
+import chaiHttp = require('chai-http');
+import { Response } from 'superagent';
+import { Sales } from '@prisma/client';
+import { app } from '../../../index';
+import SalesRepository from '../../../repositories/implementations/salesRepository';
+import { sales } from './mocks/SalesMocks';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Sales', async () => {
+  let chaiHttpResponse: Response;
+  let repositoryStub: sinon.SinonStub;
+
+  describe('Find by id sales', async () => {
+    describe('Find by id sales success', async () => {
+      beforeEach(async () => {
+        repositoryStub = sinon.stub(SalesRepository.prototype, 'findById').resolves(sales[0] as unknown as Sales);
+      });
+      afterEach(() => {
+        repositoryStub.restore();
+      });
+      it('return status 200', async () => {
+        chaiHttpResponse = await chai
+          .request(app)
+          .get('/sales/1');
+
+        expect(chaiHttpResponse.status).to.be.eql(200);
+        expect(chaiHttpResponse.body).to.be.eql(sales[0]);
+      });
+    });
+
+    describe('Find by id products falid', async () => {
+      beforeEach(async () => {
+        repositoryStub = sinon.stub(SalesRepository.prototype, 'findById').resolves(null);
+      });
+      afterEach(() => {
+        repositoryStub.restore();
+      });
+      it('return status 404', async () => {
+        chaiHttpResponse = await chai
+          .request(app)
+          .get('/products/1')
+          .send();
+        expect(chaiHttpResponse.status).to.be.eql(404);
+        expect(chaiHttpResponse.body).to.be.eql({ message: 'Sales not found' });
+      });
+    });
+  });
+
+  describe('Find all products', async () => {
+    describe('Find all products success', async () => {
+      beforeEach(async () => {
+        repositoryStub = sinon.stub(SalesRepository.prototype, 'findAll').resolves(sales as unknown as Sales[]);
+      });
+      afterEach(() => {
+        repositoryStub.restore();
+      });
+      it('return status 200', async () => {
+        chaiHttpResponse = await chai
+          .request(app)
+          .get('/products');
+
+        expect(chaiHttpResponse.status).to.be.eql(200);
+        expect(chaiHttpResponse.body).to.be.eql(sales);
+      });
+    });
+  });
+});
---
+++ src/modules/sales/tests/mocks/SalesMocks.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+const sales = [
+  {
+    id: 1,
+    date: '11/11/11',
+    Sales_Producs: [],
+  },
+];
+
+export { sales };
