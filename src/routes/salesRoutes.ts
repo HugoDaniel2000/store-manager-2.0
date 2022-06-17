bab829816a3358573f28178b5470da19df37a602
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-17 00:36:22 -0300

    feat(src/routes): create route post sales

--- src/routes/salesRoutes.ts
+++ src/routes/salesRoutes.ts
@@ -3,21 +3,31 @@ import {
 } from 'express';
 import AuthMiddleware from '../middlewares/authMiddleware';
 import FindSalesController from '../modules/sales/useCases/findSales/findSalesController';
+import SalesController from '../modules/sales/useCases/sale/saleController';
 
 const findSalesController = new FindSalesController();
-
+const salesController = new SalesController();
 const router = Router();
 
 router.route('/')
   .get(
-    // AuthMiddleware.validRoleUserAdmin,
+    AuthMiddleware.validToken,
+    AuthMiddleware.validRoleUserAdmin,
     (req: Request, res: Response, next: NextFunction) => {
       findSalesController.findAllSales(req, res, next);
+    },
+  )
+  .post(
+    AuthMiddleware.validToken,
+    AuthMiddleware.getUserIdToken,
+    (req: Request, res: Response, next: NextFunction) => {
+      salesController.createSales(req, res, next);
     },
   );
 
 router.route('/:id')
   .get(
+    AuthMiddleware.validToken,
     AuthMiddleware.validRoleUserAdmin,
     (req: Request, res: Response, next: NextFunction) => {
       findSalesController.findSalesById(req, res, next);
