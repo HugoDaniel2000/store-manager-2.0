8eab650e223fd1e990dcb4b97888c27856841e1a
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-17 16:15:32 -0300

    style(src/middlewares): remove console.log

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -3,8 +3,6 @@ import { DefinedHttpError } from 'restify-errors';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const error = (err: DefinedHttpError, req: Request, res: Response, _next: NextFunction) => {
-  console.log(err);
-
   if (err.statusCode) {
     return res.status(err.statusCode).json({ message: err.message });
   }
