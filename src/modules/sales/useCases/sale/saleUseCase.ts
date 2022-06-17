203f14e01aabe7065a1340c94ab44bdfdbfe7396
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-17 02:07:02 -0300

    feat(src/modules/sales): add conditional

--- src/modules/sales/useCases/sale/saleUseCase.ts
+++ src/modules/sales/useCases/sale/saleUseCase.ts
@@ -1,4 +1,7 @@
 import { Sales } from '@prisma/client';
+import errors from 'restify-errors';
+import ProductsRepository from '../../../../repositories/implementations/productsRepository';
+
 // import errors from 'restify-errors';
 import SalesRepository from '../../../../repositories/implementations/salesRepository';
 import { newSales } from '../../../../types/sales';
@@ -6,11 +9,19 @@ import { newSales } from '../../../../types/sales';
 export default class SaleUseCase {
   private salesRepository: SalesRepository;
 
+  private productsRepository: ProductsRepository;
+
   constructor() {
     this.salesRepository = new SalesRepository();
+    this.productsRepository = new ProductsRepository();
   }
 
   async createSales(sales: newSales): Promise<Sales> {
+    const products = sales.sales.map((sale) => this.productsRepository.findById(sale.product_id));
+    const productsExist = await Promise.all(products).then((data) => data);
+    if (productsExist.includes(null)) {
+      throw new errors.NotFoundError('Product not found');
+    }
     const salesCreated = await this.salesRepository.create(sales);
     return salesCreated;
   }
