10fb39de4704454a5918a20719a2bfa17a635c77
HugoDaniel2000 <hugodanielc67@gmail.com>
2022-06-18 18:12:23 -0300

    feat(src/repositories/interfaces): add method findByIdProductSales

--- src/repositories/interfaces/ISalesRepository.ts
+++ src/repositories/interfaces/ISalesRepository.ts
@@ -1,9 +1,10 @@
 import { Sales } from '@prisma/client';
-import { newSales, salesUpdate } from '../../types/sales';
+import { newSales, salesProducts, salesUpdate } from '../../types/sales';
 
 export interface ISalesRepository {
   findAll(): Promise<Sales[]>;
   findById(id: number): Promise<Sales | null>;
+  findByIdProductSales(id: number): Promise<salesProducts[]>;
   create(sales: newSales): Promise<Sales>;
   update(saleUpdt: salesUpdate): Promise<void>
   deleteById(id: number): Promise<void>
